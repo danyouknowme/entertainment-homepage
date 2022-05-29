37df10e7dfa78880c30b989d535eb5cf54a30ef8
Thanathip <dannajaeiei@hotmail.com>
2022-05-29 19:13:05 +0700

    Added: bootstrap-vue to Vue

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
+
 import "./index.scss";
+import 'bootstrap-vue/dist/bootstrap-vue-icons.min.css';
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
 new Vue({
-  router,
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app');
