68653417f07a95e1ebe6714a2055f15eb28b0726
Thanathip <dannajaeiei@hotmail.com>
2022-05-29 19:25:35 +0700

    Feat: searchbar component

---
+++ src/components/Search.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex w-full items-center">
+    <b-icon icon="search" class="text-white text-lg my-3 mr-4"></b-icon>
+    <input
+      type="text"
+      placeholder="Search for movies or TV series"
+      class="flex-1 outline-none border-none text-white bg-transparent"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({})
+export default class Search extends Vue {}
+</script>
+  
