40d61c1cc43fe5f6f9b9ffc25af20b96fa6bb79e
Thanathip <dannajaeiei@hotmail.com>
2022-05-29 19:12:34 +0700

    Added: colors to tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      darkblue: "#10141F",
+      lightblue: "#161D2F",
+      white: "#FFFFFF",
+      lightbluefont: "#5A6991",
+      red: "#FF4543",
+    },
   },
   plugins: [],
 };
