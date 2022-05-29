c2763e1b2db256380a609b8b724d4a3e010b0f70
Thanathip <dannajaeiei@hotmail.com>
2022-05-29 19:13:33 +0700

    Added: navbar into homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="home p-6 min-h-screen flex">
+    <Navbar :page="'home'" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import Navbar from "@/components/Navbar.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
+    Navbar,
   },
 })
 export default class HomeView extends Vue {}
