40c30123ff7893fb73ea7020ea9602d7cb82f453
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-05-30 19:21:55 +0700

    Fetching data and render card component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,19 +5,26 @@
 			<Search />
 			<!-- Trending -->
 			<div class="w-full mt-4">
-				<p class="text-2xl text-white">Trending</p>
+				<div class="flex items-center justify-between">
+					<span class="text-2xl text-white">Trending</span>
+					<div
+						class="text-sm text-lightgrey flex items-center transition-all delay-300"
+						:style="{ opacity: isScrolling ? '0' : '1' }"
+					>
+						<span>Scrolling</span>
+						<b-icon icon="arrow-right" class="ml-2 text-lg" />
+					</div>
+				</div>
 				<div
 					class="w-full overflow-x-auto gap-6 flex mt-4 horizontal-scroll"
 					ref="horizontal-scroll"
 					@mousewheel="scrollX"
 				>
-					<Card />
-					<Card />
-					<Card />
-					<Card />
-					<Card />
-					<Card />
-					<Card />
+					<Card
+						v-for="(movie, index) in trendingMovies"
+						:key="index"
+						:movie="movie"
+					/>
 				</div>
 			</div>
 		</div>
@@ -26,9 +33,11 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import Navbar from "@/components/Navbar.vue"; // @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
 import Search from "@/components/Search.vue";
 import Card from "@/components/Card.vue";
+import { Movie } from "../types/movie";
+import { ROOT_API, TMDB_API_KEY } from "../config/config";
 
 @Component({
 	components: {
@@ -38,15 +47,31 @@ import Card from "@/components/Card.vue";
 	},
 })
 export default class HomeView extends Vue {
+	trendingMovies: Movie[] | undefined = [];
+	isScrolling = false;
+
 	private scrollX(event: WheelEvent) {
 		const scrollContainer = this.$refs[
 			"horizontal-scroll"
 		] as HTMLElement | null;
 		if (!scrollContainer) return;
+		this.isScrolling = scrollContainer.scrollLeft !== 0;
 		scrollContainer.scrollLeft += event.deltaY;
-		// const scrollContainer = document.getElementById("horizontal-scroll");
-		// if (!scrollContainer) return;
-		// scrollContainer.scrollLeft += e.deltaY;
+	}
+
+	private async getTrendingMovies() {
+		const response = await fetch(
+			`${ROOT_API}/movie/top_rated?api_key=${TMDB_API_KEY}`,
+			{
+				method: "GET",
+			}
+		);
+		const data = await response.json();
+		return data.results;
+	}
+
+	async created() {
+		this.trendingMovies = await this.getTrendingMovies();
 	}
 }
 </script>
