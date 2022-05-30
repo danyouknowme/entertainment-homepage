4dd3a6655e0e86151c142d5012558fcb505f8f96
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-05-30 23:22:10 +0700

    Added: function handleChange to change search value

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="flex w-full items-center">
-    <b-icon icon="search" class="text-white text-lg my-3 mr-4"></b-icon>
-    <input
-      type="text"
-      placeholder="Search for movies or TV series"
-      class="flex-1 outline-none border-none text-white bg-transparent"
-    />
-  </div>
+	<div class="flex w-full items-center">
+		<b-icon icon="search" class="text-white text-lg my-3 mr-4"></b-icon>
+		<input
+			type="text"
+			placeholder="Search for movies or TV series"
+			class="flex-1 outline-none border-none text-white bg-transparent"
+			@input="handleChange"
+		/>
+	</div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from "vue-property-decorator";
+import { Vue, Component, Prop } from "vue-property-decorator";
 
 @Component({})
-export default class Search extends Vue {}
+export default class Search extends Vue {
+	@Prop() value!: string;
+
+	private handleChange(event: Event) {
+		const target = event.target as HTMLButtonElement;
+		if (target) {
+			this.$emit("value-change", target.value);
+		}
+	}
+}
 </script>
-  
