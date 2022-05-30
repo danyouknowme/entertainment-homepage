aff5d552e3bb556449af0922bacce18fae38ba7f
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-05-30 16:31:53 +0700

    Created: card component

---
+++ src/components/Card.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="flex-0 w-80 h-40 bg-white rounded"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Card extends Vue {}
+</script>
+
+<style scoped lang="scss"></style>
